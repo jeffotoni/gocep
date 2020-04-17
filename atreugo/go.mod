48bead6aec52e032a7d5f0bf27f6afcda4e2c03b
jefferson <jeff.otoni@gmail.com>
2020-04-17 16:40:47 -0300

    segundo commit subindo server utilizando atreugo

---
+++ atreugo/go.mod
@@ -0,0 +1,5 @@
+module atreugo
+
+go 1.14
+
+require github.com/savsgio/atreugo/v11 v11.0.2 // indirect
---
+++ atreugo/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/savsgio/atreugo/v11"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"sync"
+	"time"
+)
+
+type End struct {
+	Source string
+	Url    string
+}
+
+type Result struct {
+	Body string
+}
+
+var endpoints = []End{
+	{"viacep", "https://viacep.com.br/ws/%s/json/"},
+	{"postmon", "https://api.postmon.com.br/v1/cep/%s"},
+	{"republicavirtual", "https://republicavirtual.com.br/web_cep.php?cep=%s&formato=json"},
+}
+
+func main() {
+
+	config := atreugo.Config{
+		Addr: "0.0.0.0:8084",
+	}
+
+	server := atreugo.New(config)
+
+	v1 := server.NewGroupPath("/api/v1")
+	v1.GET("/{cep:[0-9]{8}}", func(ctx *atreugo.RequestCtx) error {
+
+		cep := fmt.Sprintf("%s", ctx.UserValue("cep"))
+
+		var mux sync.Mutex
+		var result Result
+		var wg sync.WaitGroup
+		wg.Add(3)
+		for _, e := range endpoints {
+			endpoint := fmt.Sprintf(e.Url, cep)
+			go func(endpoint string, result *Result) {
+				defer wg.Done()
+				ctx2, cancel := context.WithCancel(context.TODO())
+				afterFuncTimer := time.AfterFunc(350*time.Millisecond, func() {
+					cancel()
+				})
+				defer afterFuncTimer.Stop()
+
+				req, err := http.NewRequest("GET", endpoint, nil)
+				if err != nil {
+					log.Println("Error NewRequest:", err)
+					return
+				}
+
+				req = req.WithContext(ctx2)
+
+				response, err := http.DefaultClient.Do(req)
+				if err != nil {
+					log.Println("Error ClientDo:", err)
+					return
+				}
+
+				defer response.Body.Close()
+
+				requestContent, err := ioutil.ReadAll(response.Body)
+				if err != nil {
+					log.Println("Error ioutil.ReadAll:", err)
+					return
+				}
+
+				if len(string(requestContent)) > 0 &&
+					response.StatusCode == http.StatusOK {
+					println("entrei....::")
+					mux.Lock()
+					defer mux.Unlock()
+					result.Body = string(requestContent)
+					return
+				}
+			}(endpoint, &result)
+		}
+		wg.Wait()
+
+		return ctx.TextResponse(result.Body)
+	})
+
+	if err := server.ListenAndServe(); err != nil {
+		fmt.Println(err)
+	}
+}
